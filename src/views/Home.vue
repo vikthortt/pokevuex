48a1246c40ff58d1eedca239719ba493af3d7c18
Victor Torres <vikthort.t@gmail.com>
2020-11-09 00:42:26 -0600

    connect api data with preview component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <PokemonPreview
+      v-for="pokemon of pokemonPreviews"
+      :key="pokemon.index"
+      :pokemon="pokemon"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PokemonPreview from "@/components/PokemonPreview.vue";
+import api from "@/api/pokeapi";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    PokemonPreview,
+  },
+  data: () => ({
+    pokedex: null,
+  }),
+  computed: {
+    pokemonPreviews() {
+      return this.pokedex?.entries.map((pokemon) => ({
+        index: pokemon.id,
+        name: pokemon.name,
+        sprite: pokemon.sprite,
+        types: pokemon.types.map((t, i) => ({
+          id: i,
+          name: t,
+          classes: ["bg-gray-200"],
+        })),
+      }));
+    },
+  },
+  async created() {
+    this.pokedex = await api.preloadPokedexData();
+  },
 };
 </script>
